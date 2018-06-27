13d83e01a85e7e5353e8664ee1de0eb08655420d
onetwo7 <lahoohal@gmail.com>
2018-06-27 19:11:58 +0300

    minor changes

--- app/keys/index.js
+++ app/keys/index.js
@@ -1,11 +1,5 @@
-const { NODE_ENV: env, MODE: mode } = process.env;
-
-if (env === 'production' && mode !== 'build-test') {
+if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
-} else if (mode === 'build-test') {
-  module.exports = Object.assign({}, require('./dev'), {
-    uploadsPath: require('./prod').uploadsPath
-  });
 } else {
   module.exports = require('./dev');
 }
--- app/keys/prod.js
+++ app/keys/prod.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const uploadsPath = path.join(__dirname, '../../dist/img/uploads');
+const dev = require('./dev');
+const { env } = process;
 
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  githubClientID: process.env.GITHUB_CLIENT_ID,
-  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
-  vkClientID: process.env.VK_CLIENT_ID,
-  vkClientSecret: process.env.VK_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
+  googleClientID: env.GOOGLE_CLIENT_ID || dev.googleClientID,
+  googleClientSecret: env.GOOGLE_CLIENT_SECRET || dev.googleClientSecret,
+  githubClientID: env.GITHUB_CLIENT_ID || dev.githubClientID,
+  githubClientSecret: env.GITHUB_CLIENT_SECRET || dev.githubClientSecret,
+  vkClientID: env.VK_CLIENT_ID || dev.vkClientID,
+  vkClientSecret: env.VK_CLIENT_SECRET || dev.vkClientSecret,
+  mongoURI: env.MONGO_URI || dev.mongoURI,
+  cookieKey: env.COOKIE_KEY || dev.cookieKey,
   uploadsPath
 };
