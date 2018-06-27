b3b0254246f59936f678a92d716e86f7310c25bb
onetwo7 <lahoohal@gmail.com>
2018-06-27 20:11:19 +0300

    tiny fix

--- app/keys/prod.js
+++ app/keys/prod.js
@@ -1,16 +1,15 @@
 const path = require('path');
 const uploadsPath = path.join(__dirname, '../../dist/img/uploads');
-const dev = require('./dev');
 const { env } = process;
 
 module.exports = {
-  googleClientID: env.GOOGLE_CLIENT_ID || dev.googleClientID,
-  googleClientSecret: env.GOOGLE_CLIENT_SECRET || dev.googleClientSecret,
-  githubClientID: env.GITHUB_CLIENT_ID || dev.githubClientID,
-  githubClientSecret: env.GITHUB_CLIENT_SECRET || dev.githubClientSecret,
-  vkClientID: env.VK_CLIENT_ID || dev.vkClientID,
-  vkClientSecret: env.VK_CLIENT_SECRET || dev.vkClientSecret,
-  mongoURI: env.MONGO_URI || dev.mongoURI,
-  cookieKey: env.COOKIE_KEY || dev.cookieKey,
+  googleClientID: env.GOOGLE_CLIENT_ID,
+  googleClientSecret: env.GOOGLE_CLIENT_SECRET,
+  githubClientID: env.GITHUB_CLIENT_ID,
+  githubClientSecret: env.GITHUB_CLIENT_SECRET,
+  vkClientID: env.VK_CLIENT_ID,
+  vkClientSecret: env.VK_CLIENT_SECRET,
+  mongoURI: env.MONGO_URI,
+  cookieKey: env.COOKIE_KEY,
   uploadsPath
 };
